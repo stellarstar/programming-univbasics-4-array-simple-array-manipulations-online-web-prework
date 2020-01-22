ddd8a4efcd6c3c20bf268a0d5f117863f765d5ea
Dyla n ra y <technicalpro@securemedia.ch>
2020-01-22 01:39:17 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,49 @@
+
+def  using_push (new_array, new_string)
+new_array.push (new_string)
+end
+
+
+def using_unshift (new_array, new_string)
+new_array.unshift (new_string)
+end
+
+def using_pop(array)
+array.pop
+end
+
+def pop_with_args(array)
+array.pop (2)
+end
+
+def using_shift(array)
+array.shift  
+end
+
+def shift_with_args(array)
+array.shift (2)
+end
+
+def using_concat(array)
+array.concat 
+end
+
+def using_insert(array, item)
+new_array.unshift (new_string) 
+end
+
+def using_uniq(array)
+new_array.unshift (new_string)
+end
+
+def using_flatten(array)
+new_array.unshift (new_string) 
+end
+
+def using_delete(array, string)
+new_array.unshift (new_string) 
+end
+
+def using_delete_at(array, num)
+new_array.unshift (new_string)
+end
